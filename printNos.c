c558dbd605d666ff51fb81cb6c052e3454a468e2
Darcy <abirdutta.dgp@gmail.com>
2023-09-14 15:19:27 +0530

    initial commit

---
+++ printNos.c
@@ -0,0 +1,17 @@
+// to print number until n by user
+
+#include <stdio.h>
+
+int main(){
+    int n,i;
+     printf("Enter a number :");
+     scanf("%d",&n);
+
+     for (int i = 0; i <= n; i++)
+     {
+        printf("%d\n",i);
+     }
+     return 0;
+     
+
+}
