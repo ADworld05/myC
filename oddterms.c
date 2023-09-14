c1b04ee6fa582066cce4ba796ce55a7c005c8a24
Darcy <abirdutta.dgp@gmail.com>
2023-09-14 16:13:32 +0530

    added avg of nos

--- oddterms.c
+++ oddterms.c
@@ -1,18 +1,20 @@
-// c program to print first n odd terms 
+// c program to print first n odd terms and find their average
 
 #include <stdio.h>
 int main()
 {
-    int n;
+    int n, sum = 0;
     printf("Enter a number :");
     scanf("%d", &n);
 
-    for (int i = 0; i < 2*n; i++)  //2*n @ check in first 2n integers
+    for (int i = 0; i < 2 * n; i++) // 2*n @ check in first 2n integers
     {
         if (i % 2 != 0)
         {
             printf("%d\n", i);
+            sum = sum + i;
         }
-        
     }
+    float avg = (float)sum / n;
+    printf("the average of first %d odd number is %f", n, avg);
 }
