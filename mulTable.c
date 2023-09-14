bd8731bf00f895e67785ea106a9a5abdbab321af
Darcy <abirdutta.dgp@gmail.com>
2023-09-14 15:37:06 +0530

    added table 1-n

--- mulTable.c
+++ mulTable.c
@@ -2,11 +2,15 @@
 
 #include <stdio.h>
  int main(){
-    int num,i=1;
+    int num,i=1,col;
     printf("Enter a number to print multiplication table\n");
     scanf("%d",&num);
+    printf("Print table upto :\n");
+    scanf("%d",&col);
     
-    while (i<=10)
+    
+    
+    while (i<=col)
     {
         printf("%d * %d = %d \n",num,i,(num*i));
         i+=1;
