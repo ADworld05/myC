d78dc17da7ccb0c2e31cb71eb428b58d0e1a2b08
Darcy <abirdutta.dgp@gmail.com>
2024-01-17 22:13:23 +0530

    sample linear search

---
+++ DSA/linearSearch.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ 
+int linearSearch(int arr[], int size, int element){
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i]==element){
+            return i;
+        }
+    }
+    return -1;
+}
+ 
+int main(){
+    // Unsorted array for linear search
+    int c;
+    int arr[] = {1,3,5,56,4,3,23,5,4,54634,56,34};
+    int size = sizeof(arr)/sizeof(int);
+    c=linearSearch(arr,size,5877876);
+    printf("%d",c);
+    return 0;
+}
